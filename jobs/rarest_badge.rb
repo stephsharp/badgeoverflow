080a27989e843a3ea76d019ddf13b87a89463249
Stephanie Sharp <stephrsharp@gmail.com>
2013-08-02 22:15:04 +1000

    Rename http to stack_exchange

--- jobs/rarest_badge.rb
+++ jobs/rarest_badge.rb
@@ -7,8 +7,8 @@ user_id = 1367622
 
 # Get all badges for user - /users/{ids}/badges
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  http = Net::HTTP.new('api.stackexchange.com')
-  user_badges_response = JSON.parse(http.get("/2.1/users/#{user_id}/badges?site=stackoverflow").body)
+  stack_exchange = Net::HTTP.new('api.stackexchange.com')
+  user_badges_response = JSON.parse(stack_exchange.get("/2.1/users/#{user_id}/badges?site=stackoverflow").body)
   
   # Create array of badge ids
   badge_ids = user_badges_response['items'].map { |badge| badge['badge_id'] }
@@ -17,7 +17,7 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     send_event('rarest_badge', { :text => "No badges", :moreinfo => ""})
   else
     # Get all badges with ids - /badges/{ids} 
-    badges_response = JSON.parse(http.get("/2.1/badges/#{badge_ids.join(';')}?site=stackoverflow").body)
+    badges_response = JSON.parse(stack_exchange.get("/2.1/badges/#{badge_ids.join(';')}?site=stackoverflow").body)
   
     # Get badge with lowest award_count
     rarest_badge = badges_response['items'].reduce do |rarest, badge|
