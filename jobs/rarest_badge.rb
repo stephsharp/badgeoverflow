8b3cda30a2f8d7e632cb5155fc060a27d888c8d5
Stephanie Sharp <stephrsharp@gmail.com>
2013-08-02 21:57:06 +1000

    Basic rarest badge widget

--- jobs/rarest_badge.rb
+++ jobs/rarest_badge.rb
@@ -6,13 +6,30 @@ require 'json'
 user_id = 1367622
 
 # Get all badges for user - /users/{ids}/badges
-
-# Create array of badge ids
-
-# Get all badges with ids -  /badges/{ids} 
-
-# Get badge with lowest award_count
-
-# Display badge name with award_count below
-
-# Set background colour of widget to badge rank (bronze/silver/gold)
+SCHEDULER.every '1m', :first_in => 0 do |job|
+  http = Net::HTTP.new('api.stackexchange.com')
+  user_badges_response = JSON.parse(http.get("/2.1/users/#{user_id}/badges?site=stackoverflow").body)
+  
+  # Create array of badge ids
+  badge_ids = user_badges_response['items'].map { |badge| badge['badge_id'] }
+  
+  if badge_ids.empty?
+    send_event('rarest_badge', { :text => "No badges", :moreinfo => ""})
+  else
+    # Get all badges with ids - /badges/{ids} 
+    badges_response = JSON.parse(http.get("/2.1/badges/#{badge_ids.join(';')}?site=stackoverflow").body)
+  
+    # Get badge with lowest award_count
+    rarest_badge = badges_response['items'].reduce do |rarest, badge|
+      badge['award_count'] < rarest['award_count'] ? badge : rarest
+    end
+  
+    # Display badge name with award_count below
+    send_event('rarest_badge', { :text => rarest_badge['name'], :moreinfo => rarest_badge['award_count']})
+  
+    # Set background colour of widget to badge rank (bronze/silver/gold)
+  
+    # pagesize - what if the user has > 30 badges?
+    
+  end
+end
