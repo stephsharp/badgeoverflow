444b4ea5bc9ef28e3e7095c3da0b8cab7680449a
Stephanie Sharp <stephrsharp@gmail.com>
2013-08-02 20:52:38 +1000

    Create empty rarest_badge job

---
+++ jobs/rarest_badge.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+require 'json'
+
+# Steph's User id: 1367622 
+# Adam's user id: 1164143
+user_id = 1367622
+
+# Get all badges for user - /users/{ids}/badges
+
+# Create array of badge ids
+
+# Get all badges with ids -  /badges/{ids} 
+
+# Get badge with lowest award_count
+
+# Display badge name with award_count below
+
+# Set background colour of widget to badge rank (bronze/silver/gold)
