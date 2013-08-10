7bc960d7b0fe52eff5ca99c884901a6d89fe7dcf
Adam Sharp <adam.sharp@outware.com.au>
2013-08-10 18:09:14 +1000

    Update avatar styling/layout according to design

--- jobs/user.rb
+++ jobs/user.rb
@@ -1,12 +1,19 @@
 user_id = 1164143
+avatar_size = 230
 
 SCHEDULER.every '5m', :first_in => 0 do |job|
   include Service
 
   StackOverflow.fetch 'users', ids: user_id do |user|
+    profile_image = user['profile_image']
+
+    if profile_image.include? 'gravatar.com'
+      profile_image << "&size=#{avatar_size}"
+    end
+
     send_event 'user', {
-      image: user['profile_image'],
-      width: 75
+      image: profile_image,
+      width: avatar_size
     }
   end
 end
