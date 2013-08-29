aedca73ca45efc6aa35698d130aa0d75b9b1cbc0
Stephanie Sharp <stephrsharp@gmail.com>
2013-08-29 21:11:43 +1000

    Implement Altruist badge class

--- lib/models/badges/altruist.rb
+++ lib/models/badges/altruist.rb
@@ -3,4 +3,9 @@ require_relative 'badge'
 class Altruist < Badge
   # add callbacks here to return progress percentage, progress string,
   # to fetch required data, etc.
+
+  def progress_description
+    "Manually award a bounty on another person's question"
+  end
+
 end
