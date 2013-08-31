35485a5b81c59741d296754bdf9a9acafd14e467
Adam Sharp <adam.sharp@outware.com.au>
2013-08-31 19:01:53 +1000

    Remove trailing whitespace Steph shouldn't have committed

--- lib/models/badges/questions/favorite_question.rb
+++ lib/models/badges/questions/favorite_question.rb
@@ -15,26 +15,26 @@ class FavoriteQuestion < Badge
       filter: '!9cC8zbO*T',
       fetch_all_pages: true
     })
-    
-    # If only 1 question is returned, it is a Hash, not an array of Hashes 
+
+    # If only 1 question is returned, it is a Hash, not an array of Hashes
     if (!questions.is_a? Array)
       questions = Array.new [questions]
     end
-    
+
     if (questions.length > 0)
       highest_favorites_question = questions.first
-      
+
       # Get badge with highest favorite_count
       if (questions.length > 1)
         highest_favorites_question = questions.reduce do |highest_favorites, question|
           question['favorite_count'] > highest_favorites['favorite_count'] ? question : highest_favorites
         end
       end
-      
+
       title = highest_favorites_question['title']
       favorites = highest_favorites_question['favorite_count']
       remaining = required_favorites - favorites
-    
+
       "Your question \"#{title.truncate(70)}\" has #{favorites} favorites. #{remaining} favorites to go!"
     else
       "Question favorited by #{required_favorites} users. You have not asked any questions yet!"
