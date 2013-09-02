3ea34fb2ec915e747d64945a1ab276aa03585e95
Stephanie Sharp <stephrsharp@gmail.com>
2013-09-02 21:46:08 +1000

    Add link to answer in NiceAnswer's progress_description

--- lib/models/badges/answers/nice_answer.rb
+++ lib/models/badges/answers/nice_answer.rb
@@ -13,19 +13,21 @@ class NiceAnswer < Badge
       sort: 'votes',
       order: 'desc',
       pagesize: 1,
+      filter: '!9cC8zqAGM',
       fetch_all_pages: false
     }).first
 
     question = service.fetch('questions', ids: answer['question_id']).first
 
     question_title = question['title']
+    link = answer['link']
     score = answer['score']
     remaining = required_score - score
 
     score_str = "#{score} " + "vote".pluralize(score, "votes")
     remaining_str = "#{remaining} " + "vote".pluralize(remaining, "votes")
 
-    "Your answer on \"#{question_title.truncate(65)}\" has #{score_str}. #{remaining_str} to go!"
+    "Your answer on \"#{question_title.truncate(65).link_to(link)}\" has #{score_str}. #{remaining_str} to go!"
   end
 
   def required_score
