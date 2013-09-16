1fdb4531d2376f347d9243512a6b4f238f4d20d6
Adam Sharp <adam.sharp@outware.com.au>
2013-09-16 20:50:25 +1000

    Update badgeoverflow-core, use StackOverflow::Badge instead of just Badge

--- jobs/unearned_badges.rb
+++ jobs/unearned_badges.rb
@@ -1,7 +1,7 @@
 require 'badgeoverflow/core'
 require 'badgeoverflow/helpers'
 
-class Badge
+class StackOverflow::Badge
   include RankColour
 end
 
@@ -16,16 +16,16 @@ SCHEDULER.every '10m', :first_in => 0 do
     pagesize: 100
   }
 
-  named_badges.map! { |b| Badge.new(b, user_id) }
+  named_badges.map! { |b| StackOverflow::Badge.new(b, user_id) }
 
   user_badges = service.fetch('users', 'badges', ids: user_id)
-  user_badges.map! { |b| Badge.new(b, user_id) }
+  user_badges.map! { |b| StackOverflow::Badge.new(b, user_id) }
 
   # get unearned badges
   #
   # where there is a set of badges (bronze, silver, gold),
   # only keep lowest ranked unearned badge in array
-  unearned_badges = Badge.first_badges_in_series(named_badges - user_badges)
+  unearned_badges = StackOverflow::Badge.first_badges_in_series(named_badges - user_badges)
 
   random_badge = unearned_badges.sample
   random_badge.calculate_progress!
