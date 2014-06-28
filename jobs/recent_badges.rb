90e779108c28e6dab11165dff0320558b2cc79bf
Stephanie Sharp <stephrsharp@gmail.com>
2014-06-28 20:59:38 +1000

    WIP: recent_badges.rb

--- jobs/recent_badges.rb
+++ jobs/recent_badges.rb
@@ -10,35 +10,25 @@ user_id = BadgeOverflowConfig.user_id
 SCHEDULER.every '1h', :first_in => '90s' do
   recent_badges = []
   number_of_badges = 5
-  page_number = 1
   
-  loop do
-    user_timeline_response = service.fetch 'users', 'timeline', {
-      ids: user_id,
-      page: page_number,
-      pagesize: 100,
-      fetch_all_pages: false
-    }
+  user_timeline_response = service.fetch 'users', 'timeline', {
+    ids: user_id,
+    pagesize: 100,
+    limit: number_of_badges,
+    where: ->(item) { item['timeline_type'] == "badge" }
+  }
 
-    # Get badges in timeline
-    user_timeline_response.each do |item|
-      if item['timeline_type'] == "badge" && recent_badges.length < number_of_badges
-        badge_response = service.fetch 'badges', ids: item['badge_id']
-        badge_rank = badge_response.first['rank']
-        recent_badges << { rank: badge_rank, label: item['detail'] }
-
-        backoff = badge_response['backoff']
-        if backoff
-          sleep backoff
-        end
-      end
+  # Get badges in timeline
+  user_timeline_response.each do |item|
+    if item['timeline_type'] == "badge" && recent_badges.length < number_of_badges
+      badge_response = service.fetch 'badges', ids: item['badge_id']
+      badge_rank = badge_response.first['rank']
+      recent_badges << { rank: badge_rank, label: item['detail'] }
     end
+  end
 
-    page_number += 1
-
-    # break loop unless there are more pages to search and not enough badges have been found
-    break unless (user_timeline_response['has_more'] && recent_badges.length < number_of_badges)
-  end  
+  # break loop unless there are more pages to search and not enough badges have been found
+  # break unless (user_timeline_response['has_more'] && recent_badges.length < number_of_badges)
 
   # Check if user has no badges
   if recent_badges.empty?
