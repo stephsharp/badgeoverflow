96458ad3e8d7cf428f9c1d73ece1c4e8d9b5d6eb
Stephanie Sharp <stephrsharp@gmail.com>
2014-07-05 21:57:17 +1000

    Create site job

---
+++ jobs/site.rb
@@ -0,0 +1,19 @@
+require 'badgeoverflow/core'
+
+service = StackExchangeService.new
+
+# filter '!SmOhH*oSkO4VKR5084' (not working): high_resolution_icon_url, api_site_parameter, name
+# filter '!SmOhH.Dupn)h3uctFd': high_resolution_icon_url, icon_url, api_site_parameter, name
+service.fetch 'sites', {
+  filter: '!SmOhH.Dupn)h3uctFd',
+  fetch_all_pages: true
+} do |sites|
+  site = sites.find { |site|
+    site['api_site_parameter'] == service.site
+  }
+
+  send_event 'site', {
+    name: site['name'],
+    icon: site['high_resolution_icon_url']
+  }
+end
